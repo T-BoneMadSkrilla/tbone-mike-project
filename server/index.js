8b41188376b4eca83f3fa1e3e0ae161b393914fe
unknown <tylereftink@gmail.com>
2020-04-14 14:36:14 -0500

    landing page

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+require('dotenv').config({path: '../.env'});
+const express = require("express");
+const cors = require('cors');
+const session = require('express-session');
+const {json} = require("body-parser");
+const massive = require("massive");
+const sc = require('./controllers/studentController');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(json());
+
+const { SESSION_SECRET, SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive({
+connectionString: CONNECTION_STRING,
+ssl: {rejectUnauthorized: false}
+})
+.then(db => {
+    app.set("db", db);
+    console.log(app.get('db').listTables());
+    console.log("db connected")
+})
+.catch(err => console.log(err));
+
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+  })
+);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+});
