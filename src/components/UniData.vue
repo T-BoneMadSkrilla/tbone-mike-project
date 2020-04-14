b5d2149947157374c7f5ad6e9e5da6022dfa0a75
unknown <tylereftink@gmail.com>
2020-04-13 23:40:47 -0500

    progress

--- src/components/UniData.vue
+++ src/components/UniData.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-        <header class="header">
-            <router-link to="/universities/data"> Metrics </router-link>
-        </header>
+        Uni Data
     </div>
 </template>
 
@@ -11,23 +9,3 @@ export default {
     name: "UniData"
 }
 </script>
-
-<style scoped>
-    .header {
-        background: #34495E;
-        color: #41B883;
-        text-align: center;
-        font-size: 1.25em;
-        padding: 15px;
-    }
-
-    .header a {
-        color: white;
-        text-decoration: none;
-    }
-
-    .header a.router-link-exact-active{
-        color: #42b983;
-    }
-
-</style>
--- src/components/layout/UniNav.vue
+++ src/components/layout/UniNav.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        Uni Nav
-        <div class="alarm"></div>
+        <header class="header">
+            <router-link to="/universities/data"> Metrics </router-link>
+        </header>
     </div>
 </template>
 
@@ -10,3 +11,23 @@ export default {
     name: "UniNav"
 }
 </script>
+
+<style scoped>
+    .header {
+        background: #34495E;
+        color: #41B883;
+        text-align: center;
+        font-size: 1.25em;
+        padding: 15px;
+    }
+
+    .header a {
+        color: white;
+        text-decoration: none;
+    }
+
+    .header a.router-link-exact-active{
+        color: #42b983;
+    }
+
+</style>
