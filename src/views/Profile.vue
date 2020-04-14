ff1189a4cf7e9b89c7ac69137856c9aa4c01b552
unknown <tylereftink@gmail.com>
2020-04-14 00:26:35 -0500

    progress

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,36 @@
 <template>
-    <div>
+    <div class="relative">
+        <router-link to="/" class="homeArrow">
+        <font-awesome-icon icon="arrow-left" size="3x" />
+        </router-link>
+
+        <div class="userImg">
         Profile
-        <font-awesome-icon icon="fa-arrow-left" />
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .relative{
+        position: relative;
+    }
+
+    .homeArrow{
+        position: absolute;
+        left: 30px;
+        top: 30px;
+        color: grey;
+        height: 50px;
+        width: 50px;
+    }
+
+    .userImg{
+        margin: 0 auto;
+        margin-top: 30px;
+        height: 350px;
+        width: 350px;
+        border-radius: 50%;
+        border-style: solid;
+    }
+
+</style>
