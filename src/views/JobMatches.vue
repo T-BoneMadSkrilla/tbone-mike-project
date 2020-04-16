c1f0549912fbb5a3a5db7b6db424d5c91b45a7f2
Tyler Eftink <42481212+T-BoneMadSkrilla@users.noreply.github.com>
2020-04-16 14:03:16 -0500

    added data to candidates

--- src/views/JobMatches.vue
+++ src/views/JobMatches.vue
@@ -5,26 +5,26 @@
 
     <div v-for="job in matchedJobs" :key="job.id">
       <div class="jobCard">
-      <h4 class="companyPosition">
-        {{ job.company }} -
-        {{ job.position }}
-      </h4>
-      <div>
-        <button class="xBtn">
-          X
-        </button>
-        <button class="applyBtn">
-          Apply
-        </button>
-      </div>
+        <h4 class="companyPosition">
+          {{ job.company }} -
+          {{ job.position }}
+        </h4>
+        <div>
+          <button class="applyBtn">
+            Apply Here
+          </button>
+          <button class="xBtn">
+            Not Interested
+          </button>
+        </div>
       </div>
       <div class="descFlex">
-        <div v-if="job.descShow == false" v-on:click="openOrCloseDesc(job)">
+        <div class="icon" v-if="job.descShow == false" v-on:click="openOrCloseDesc(job)">
           Description: <font-awesome-icon icon="angle-down" />
         </div>
 
         <div v-if="job.descShow">
-          <div v-on:click="openOrCloseDesc(job)">
+          <div class="icon" v-on:click="openOrCloseDesc(job)">
             Close <font-awesome-icon icon="angle-up" />
           </div>
           <p>
@@ -50,12 +50,14 @@ export default {
   data() {
     return {
       matchedJobs: [
-        {company: "Michael-soft",
+        {id: 1,
+         company: "Michael-soft",
          position: "Code Monkey",
          descShow: false,
          description: "oh hey I'm Mike, you should work at my company",
          applied: 0},
-        {company: "Some Tech Company",
+        {id: 2,
+         company: "Some Tech Company",
          position: "Full Stack Engineer",
          descShow: false,
          description: "oh hey I'm Mike, you should work at my company. vpauosifgvbasod paosdiuhva saposdiughvba posdinpausdhv l;lkkdjf poauisdhvopasdg",
@@ -69,10 +71,10 @@ export default {
     });
   },
   methods: {
-            openOrCloseDesc(job) {
-                job.descShow = !job.descShow;
-            }
-        }
+    openOrCloseDesc(job) {
+      job.descShow = !job.descShow;
+    }
+  }
 };
 </script>
 
@@ -81,40 +83,48 @@ export default {
     margin: 0 auto;
     margin-top: 10px;
     background: #e8e8e8;
-    width: 600px;
+    width: 700px;
     display: flex;
     justify-content: space-between;
 	  align-items: center;
   }
   .companyPosition{
-    margin-left: 35px;
+    margin-left: 30px;
   }
 
   .xBtn{
     margin-right: 5px;
     height: 30px;
+    width: 100px;
     background-color: #f44336;
     border: none;
     color: white;
     text-align: center;
     text-decoration: none;
-    font-size: 13px;  
+    font-size: 13px; 
+    cursor: pointer; 
   }
   .applyBtn {
-    margin-right: 5px;
+    margin-right: 15px;
     height: 30px;
+    width: 100px;
     background-color: #4caf50;
     border: none;
     color: white;
     text-align: center;
     text-decoration: none;
     font-size: 13px;
+    cursor: pointer;
   }
   .descFlex{
     margin: 0 auto;
     display: flex;
     flex-direction: column;
     background: #e8e8e8;
-    width: 600px;
+    width: 700px;
+  }
+
+  .icon{
+    cursor: pointer;
   }
 </style>
