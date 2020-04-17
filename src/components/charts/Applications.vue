698d5c558641ae0e1be64398b0980f5ce1e1a8e9
Tyler Eftink <42481212+T-BoneMadSkrilla@users.noreply.github.com>
2020-04-16 21:03:30 -0500

    data added

---
+++ src/components/charts/Applications.vue
@@ -0,0 +1,30 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  data: () => ({
+    chartdata: {
+      labels: ['Jan','Feb', 'Mar', 'April', 'May'],
+      datasets: [
+        {
+          label: '# of applications submitted',
+          backgroundColor: '#41b883',
+          data: [20, 40, 20, 80, 100]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style>
+</style>
